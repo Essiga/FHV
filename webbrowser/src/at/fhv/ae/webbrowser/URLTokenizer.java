5c0ef822c3e4558c7ac16f349829240754090ffe
Adrian Essig <Adrian.Essig@gantner-instruments.com>
2021-10-12 17:52:08 +0200

    switch to pc

--- webbrowser/src/at/fhv/ae/webbrowser/URLTokenizer.java
+++ webbrowser/src/at/fhv/ae/webbrowser/URLTokenizer.java
@@ -2,5 +2,17 @@ package at.fhv.ae.webbrowser;
 
 public class URLTokenizer {
 
+    private String url;
+    private String separator;
 
+    public URLTokenizer(String url, String separator) {
+        this.url = url;
+        this.separator = separator;
+    }
+
+    public String tokenize(){
+
+
+        return "";
+    }
 }
